51ce6693f1ea4084514ee739af2849089ba4992f
Molerog <moltorger@gmail.com>
2024-09-24 11:30:02 +0200

    Configurado servidor y escuchando

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
 
+
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>{
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+});
+
