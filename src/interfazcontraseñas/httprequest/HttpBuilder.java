fb533a2a6122395ea4f27d10cef49ebcc10f7d71
Molerog <moltorger@gmail.com>
2024-09-25 11:56:30 +0200

    Conexion entre el cliente Java y el servidor node.js aplicada

---
+++ src/interfazcontraseñas/httprequest/HttpBuilder.java
@@ -0,0 +1,28 @@
+package interfazcontraseñas.httprequest;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpBuilder {
+    
+    //Creamos la instancia HttpClient (El sistema que envía la petición).
+    private HttpClient client = HttpClient.newHttpClient();
+    
+    //Método que devuelve el builder ya configurado con la URL.
+    private HttpRequest.Builder getRequestBuilder(String uri){
+        return HttpRequest.newBuilder().uri(URI.create(uri));
+                
+    }
+    //Método para enviar la petición al servidor
+    public void sendRequest(String uri) throws Exception {
+        HttpRequest.Builder builder = getRequestBuilder(uri); //Aquí obtenemos el builder
+        
+        HttpRequest request = builder.GET().build();
+        
+        HttpResponse <String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        System.out.println(response.body());
+    }
+    
+}
