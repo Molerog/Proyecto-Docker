7f8bedc42bafca6de1e88e6f63e0529d04e2e86e
Molerog <moltorger@gmail.com>
2024-09-30 17:13:47 +0200

    simbolos ampliados para contraseña

--- src/python/service/generatePassword.py
+++ src/python/service/generatePassword.py
@@ -5,7 +5,7 @@ def generar_contrasena():
     mayusculas = [chr(i) for i in range(65, 91)]  # Letras mayúsculas (A-Z)
     minusculas = [chr(i) for i in range(97, 123)]  # Letras minúsculas (a-z)
     numeros = [chr(i) for i in range(48, 58)]  # Números (0-9)
-    simbolos =  ['@', '*', '_', '^']  # Símbolos especiales
+    simbolos =  ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '|', ':','?', '[', ']']  # Símbolos especiales
 
     # Combina todos los caracteres posibles en una lista (en python se pueden sumar arrays sin usar métodos)
     todos_caracteres = mayusculas + minusculas + numeros + simbolos
